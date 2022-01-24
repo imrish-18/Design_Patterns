284182cc0741531253283a07abba779533220887
Rishabh Sharma <rish.sharma9012@gmail.com>
2022-01-24 11:02:13 +0530

    commit

--- src/com/java/EmployeeSorting.java
+++ src/com/java/EmployeeSorting.java
@@ -2,10 +2,15 @@ package com.java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -86,6 +91,7 @@ public class EmployeeSorting{
 		// TODO Auto-generated method stub
 		List<EmployeeSorting> employeeList = new ArrayList<EmployeeSorting>();
         
+		
 		employeeList.add(new EmployeeSorting(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
 		employeeList.add(new EmployeeSorting(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
 		employeeList.add(new EmployeeSorting(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
@@ -93,21 +99,29 @@ public class EmployeeSorting{
 	employeeList.add(new EmployeeSorting(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
       
  employeeList.sort(Comparator.comparing(EmployeeSorting::getId).thenComparing(EmployeeSorting::getSalary));
-
-	Optional<EmployeeSorting> maxSalaryEmp = 
-            employeeList.stream()
-            .collect(Collectors.maxBy(Comparator.comparing(EmployeeSorting::getSalary)));
-	System.out.println("Employee with max salary:"
-            + (maxSalaryEmp.isPresent()? maxSalaryEmp.get():"Not Applicable"));
-	 Optional<EmployeeSorting> minAgeEmp =    
-	            employeeList.stream()
-	            .collect(Collectors.minBy(Comparator.comparing(EmployeeSorting::getAge)));
-	    System.out.println("Employee with min age:"
-	            + (minAgeEmp.isPresent()? minAgeEmp.get():"Not Applicable"));
-	    double maxSalary = employeeList.stream()
-				 .map(EmployeeSorting::getSalary)
-				 .max(Double::compare).get();
-	    System.out.println(maxSalary);
+ Map<Integer, EmployeeSorting> map =employeeList.stream().collect(Collectors.toMap(EmployeeSorting::getId,Function.identity()));
+//	Optional<EmployeeSorting> maxSalaryEmp = 
+//            employeeList.stream()
+//            .collect(Collectors.maxBy(Comparator.comparing(EmployeeSorting::getSalary)));
+//	System.out.println("Employee with max salary:"
+//            + (maxSalaryEmp.isPresent()? maxSalaryEmp.get():"Not Applicable"));
+//	 Optional<EmployeeSorting> minAgeEmp =    
+//	            employeeList.stream()
+//	            .collect(Collectors.minBy(Comparator.comparing(EmployeeSorting::getAge)));
+//	    System.out.println("Employee with min age:"
+//	            + (minAgeEmp.isPresent()? minAgeEmp.get():"Not Applicable"));
+//	    double maxSalary = employeeList.stream()
+//				 .map(EmployeeSorting::getSalary)
+//				 .max(Double::compare).get();
+//	    System.out.println(maxSalary);t
+        
+ 
+	    System.out.println("values in map");
+	    System.out.println(map);
+	    Set<Entry<Integer, EmployeeSorting>> entry=map.entrySet();
+	   
+	    map.entrySet().stream().forEach(res->System.out.println(res.getKey()+" "+res.getValue()));;
+	    
 
 	}
 
